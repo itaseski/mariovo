25519d1711a3063ce6d1e2d158de5ba28a76eca8
itaseski <itaseski13@gmail.com>
2021-04-21 14:04:14 +0200

    show 127.0.0.1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vilages/', include('vilages.urls')),
+    # path('', views.home, name='home'),
+    path('', include('vilages.urls')),
 ]
--- mysite/vilages/urls.py
+++ mysite/vilages/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'vilages'
 urlpatterns = [
-    # ex: /vilages/
     path('', views.index, name='index'),    
 ]
