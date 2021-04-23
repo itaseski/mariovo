1b5ad3f81b15ca02a65ac669fd58055056d507d1
itaseski <itaseski13@gmail.com>
2021-04-23 12:15:23 +0200

    distanse fiels

--- mysite/vilages/models.py
+++ mysite/vilages/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 # Ограноци, врски и патишта кои поврзуваат се нарекуваат цдBranch, link and connecting roads, called Class-B roads, have three-digit numbers above 130.
 class LocalRoud(models.Model):
-    code = models.CharField(max_length=5)  # L80522
+    edge = models.CharField(max_length=5)  # L80522
     steam = models.CharField(max_length=4)  # R1107
+    distance = models.IntegerField(max_length=3)
